3dc89397931b85a75788f599a84a1e11d5ee507b
Tonny <musembitonnyj@gmail.com>
2022-03-03 12:33:33 +0300

    add

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,6 +1,5 @@
 <script setup>
 // import AppVue from '../App.vue';
-
 </script>
 
 <template>
@@ -20,18 +19,23 @@
   </div>
   <div col="10">
     <label>Name</label>
-    <input type="text" class="p-2 border rounded"/>
-    <p/>
+    <input type="text" class="p-2 border rounded" />
+    <p />
   </div>
-  <div class="col-10">
-  <label class="block">
-  <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-    Email
-  </span>
-  <input type="email" name="email" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com" />
-</label>
-</div>
-<div>
-    
-</div>
+  <div class="col-4">
+    <label class="block">
+      <span
+        class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700"
+      >
+        Email
+      </span>
+      <input
+        type="email"
+        name="email"
+        class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+        placeholder="you@example.com"
+      />
+    </label>
+  </div>
+  <div></div>
 </template>
